997d838e32e60172cbde02c2c44ddae6f8dda546
arlindz <arlindz123@gmail.com>
2022-12-24 20:27:42 +0000

    changed the way the preferences navbar looks

--- products.js
+++ products.js
@@ -1,9 +1,11 @@
 
 window.addEventListener('load',function(){
     for(let i = 1; i <= 7; i++){
-    document.getElementById('preference'+i).addEventListener('click',function(){
-       const element = document.getElementById('preference'+i);
-       if(element.style.border == '4px solid rgb(50, 50, 50)')element.style.border = '4px solid green';
-       else element.style.border = '4px solid rgb(50, 50, 50)';
-    });
-}});
+        document.getElementById('preference'+i).addEventListener('click',function(){
+              const element = document.getElementById('preference'+i);
+              if(element.style.backgroundColor == 'whitesmoke')
+              element.style.backgroundColor = 'gray';
+              else element.style.backgroundColor = 'whitesmoke';
+        });
+      }
+});
